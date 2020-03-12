06a7ca0443d6efd116b7c64abda0ec0d09c8d1fa
hutchcrowley <tothecoda@gmail.com>
2020-03-11 20:14:18 -0500

    Init Commit

---
+++ src/api/fetchshow.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+import axios from "axios";
+
+export const fetchShow = () => {
+  const fetchShow = () => {
+    return axios.get.then(res => {
+      return res;
+    });
+  };
+};
